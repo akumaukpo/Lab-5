284e89f1a069002331924ec27852a50f6a0ba258
johnn_000 <johnn_000@10.0.0.10>
2014-12-06 13:55:08 -0500

    Lab 5 initial push

---
+++ CISC181.PokerEngine/src/poker/PlayHand.java
@@ -0,0 +1,50 @@
+package poker;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import util.HibernateUtil;
+
+import org.hibernate.*;
+
+public class PlayHand {
+
+	public static void main(String[] args) {
+
+
+
+		for (int gCount = 0; gCount <= 100000; gCount++) {
+			ArrayList<Hand> Hands = new ArrayList<Hand>();
+			Deck d = new Deck();
+			
+			Hand h = new Hand(d);
+			
+			h.EvalHand();
+			
+			Collections.sort(Hands, Hand.HandRank);
+			
+			
+			Session session = HibernateUtil.getSessionFactory().openSession();
+			 
+			session.beginTransaction();
+			
+			Pokerresults presults = new Pokerresults();
+			presults.setHandstrength(h.getHandStrength());
+			presults.setHihand(h.getHighPairStrength());
+			presults.setLohand(h.getLowPairStrength());
+			presults.setKicker(h.getKicker());
+			
+			
+			int resultid = (Integer) session.save(presults);
+			
+			System.out.println(resultid);
+			
+			session.getTransaction().commit();
+
+			
+
+		}
+
+	}
+
+}
